dd823df20bf9d40b2a84831b29538a794714df27
yasminemagdy <yasmenmagdy640@gmail.com>
2021-03-07 20:29:05 +0400

    importing action func

--- travel/src/Client/index.js
+++ travel/src/Client/index.js
@@ -5,6 +5,6 @@ Your index.js file inside the client folder should import the main function of y
 */
 
 import '../Client/styles/main.scss'
-import {action} from './js/app'
+import {action} from './js/app.js'
 
 export {action}
